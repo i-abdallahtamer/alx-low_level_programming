a2ca475ff0cc974da8f8275f76b819b5f84eee79
Smart Monday <uchemonday01@gmail.com>
2022-06-02 10:05:55 -0700

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,10 +11,10 @@ int main(void)
 {
 char str1[70];
 
-strcpy (str1, "\"Programming is like building a multilingual puzzle");
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
 
 puts(str1);
 
 
-return(0);
+return (0);
 }
