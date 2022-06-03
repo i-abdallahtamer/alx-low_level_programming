69a2e9a3fb5149f43d43daa379ed1e604792efed
Smart Monday <uchemonday01@gmail.com>
2022-06-03 14:50:16 -0700

    print whether the number stored in the variable n is positive or negative.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n < 0)
 {
-	printf( "%d is Negative \n", n );
+	printf( "%d is negative \n", n );
 }
 else if (n > 0)
 {
-	printf("%d  is Positive \n", n);
+	printf("%d  is positive \n", n);
 }
 else
 {
-	printf("%d is Zero\n", n);
+	printf("%d is zero\n", n);
 }
 return (0);
 }
