57c963bb614739a180d1f76205259e00a4e3ed44
Smart Monday <uchemonday01@gmail.com>
2022-06-03 18:28:31 -0700

    All lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char ch = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+lower = tolower(ch);
+putchar(ch);
+
+return (0);
+}
