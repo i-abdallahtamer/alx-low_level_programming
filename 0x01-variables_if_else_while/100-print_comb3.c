df4dac1abcc63f3da4460a3164711e4d233c3cd0
Smart Monday <uchemonday01@gmail.com>
2022-06-05 17:14:25 -0700

    Write a program that prints all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int n;
+
+for (n = 0 ; n < 100 ; n++)
+{
+putchar(n / 10 + '0');
+putchar(n % 10 + '0');
+if (n < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+}
