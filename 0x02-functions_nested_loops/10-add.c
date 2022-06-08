11fb4e0145d953976ff2419f36389842057b48fa
Smart Monday <uchemonday01@gmail.com>
2022-06-08 10:45:47 -0700

    write a function that adds two integers and return result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,9 +4,9 @@
 /**
 * main - Entry point
 *
-* Description: '2 parameters 'num1;,'num2', 1 function  ad
+* Description: a function that adds two number and return the output 
 * @add: function call
-* @num1: first int parameter 
+* @num1: first int parameter
 * @num2: secont int parameter
 * Return: Always  Plus  (Success)
 */
