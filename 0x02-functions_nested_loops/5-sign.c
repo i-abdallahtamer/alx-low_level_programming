3b005e17bec292f15ec27bf667cbaeb4a14b0fd9
Smart Monday <uchemonday01@gmail.com>
2022-06-07 19:48:29 -0700

    Write a function that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,20 @@ int print_sign(int n)
 
 if (n > 0)
 {
-return (1);
+
 _putchar(75);
+return (1);
 }
 else if (n < 0)
 {
-return(-1);
+
 _putchar(77);
+return (-1);
 }
 else
-{
-return (0);
+}
+
 _putchar(80);
+return (0);
 }
 }
