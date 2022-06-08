e378d13c709cd9aff43965b2429a8851cd0007b6
Smart Monday <uchemonday01@gmail.com>
2022-06-07 20:19:14 -0700

    Write a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isalpha - checks for alphabet
+* @c: the character to check
+* Return: 1 if c is lowercase,or upper,  0 otherwise
+*/
+
+
+int add(int, int)
+{
+int  num1;
+int num2;
+add = num1 + num2;
+printf("%d\n", add);
+}
