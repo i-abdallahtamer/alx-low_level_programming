a630da8d20498649ff5a942b7c39d794fa979610
Smart Monday <uchemonday01@gmail.com>
2022-06-08 12:37:39 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,23 +3,22 @@
 
 /**
 * main - Entry point
-* add - returns the sum of two numbers
+*times_table - return 9 times multiplication table
 * Return: Always  Plus  (Success)
 */
 void times_table(void)
 {
-int i ;
-int j ;
+int i;
+int j;
 int mult;
 for (j = 0; j <= 9; j++)
 {
 
-for  (i = 0;  i <= 9; i++)
+for (i = 0;  i <= 9; i++)
 {
- mult = i * j;
-printf ("%d, ", mult);
+mult = i * j;
+printf("%d, ", mult);
 }
-printf ("\n");
- 
+printf("\n");
 }
 }
