7029b014b77170ff9eafc1fb5fb20ebe45cc31f2
Smart Monday <uchemonday01@gmail.com>
2022-06-07 18:44:48 -0700

    Write a function that checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet - Entry point
+* Description: prints the alphabet in small caps
+* Return: Always 0 (Success)
+*/
+
+int _islower(int c)
+[
+
+if (c == 'c')
+{
+return (1);
+}
+else
+{
+return (0);
+}
