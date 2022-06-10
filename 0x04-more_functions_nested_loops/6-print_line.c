8e9e213d604674b7cf566761f49bdbb03d3e3eda
Smart Monday <uchemonday01@gmail.com>
2022-06-09 20:35:37 -0700

    print line'

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_most_numbers - print the numbers from 0 to 9
+* except 2 and 4
+* @ch; variable to hold numbers
+* Return: void
+*/
+
+void print_line(int n)
+{
+int i = 0;
+while (i < n)
+{
+__putchar('_');
+i++;
+}
+_putchar('\n');
+}
