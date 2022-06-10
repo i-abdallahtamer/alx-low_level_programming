08b46047ac2d9feb3aaa07d5a9edd8efff180a4f
Smart Monday <uchemonday01@gmail.com>
2022-06-09 17:24:32 -0700

    function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - multiply two mumbers
+*
+* @a: first parameter
+* @b: second parameter
+*
+* Return: multiple of two number
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+
+}
