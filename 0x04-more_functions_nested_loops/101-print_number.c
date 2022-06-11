7ad0beda5e581b28150698029c975ce099f4ae82
Smart Monday <uchemonday01@gmail.com>
2022-06-10 18:41:49 -0700

    print number using put char

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_number - print number using putchar
+* @n: parameter hold  numbers
+* Return: void
+*/
+
+void print_number(int n)
+{
+if (n >= 10)
+{
+_putchar(ch / 10 + '0');
+}
+else
+{
+_putchar(ch % 10  + '0');
+
+}
+
+}
