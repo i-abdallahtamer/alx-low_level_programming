c024394cd89903e395976dfbc183694b953013cc
Smart Monday <uchemonday01@gmail.com>
2022-06-13 14:06:39 -0700

    Write a function that returns the length of a string.

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,21 @@
 /**
  * _strlen - return the length of a string
  * @s : Parameter accept  string
+ * @count: store count value
  * Return: length of string
  *
  */
 
-int _strlen(char *s);
+int _strlen(char *s)
 {
-int length = *s
+int b;
+int count = 0;
 
+for (b = 0; s[b] != '\0'; b++)
+{
+
+count += 1;
+}
+_putchar(count);
+return(0);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,3 +2,5 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void print_array(int *a, int n);
+void rev_string(char *s);
+int _strlen(char *s);
