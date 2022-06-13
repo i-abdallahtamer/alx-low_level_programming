5f811cc1421198bf5e2046393fa0c77d994786d4
Smart Monday <uchemonday01@gmail.com>
2022-06-13 10:31:22 -0700

    function that prints n elements of an array of integers, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s : Parameter accept  string
+ * Return: length of string
+ *
+ */
+
+int _strlen(char *s);
+{
+int length = *s
+
+}
