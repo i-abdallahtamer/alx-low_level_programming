2bc6bcd238a9cee7cfac348153d9b4934aa0cfc4
Smart Monday <uchemonday01@gmail.com>
2022-06-13 21:56:04 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,14 @@
-void _puts(char *str);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 int _putchar(char c);
+void _puts(char *str);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void print_array(int *a, int n);
 void rev_string(char *s);
 int _strlen(char *s);
 void print_rev(char *s);
+
+#endif /* MAIN_H */
