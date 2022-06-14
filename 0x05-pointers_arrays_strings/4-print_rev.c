427cc08273de1b5c6403afa338cb30c3311870e6
Smart Monday <uchemonday01@gmail.com>
2022-06-13 20:27:19 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * rev_string - return the length of a string
+ * print_rev - return the length of a string
  * @s : Parameter accept  string
  * Return: nothng
  *
  */
 
-void rev_string(char *s)
+void print_rev(char *s)
 {
 int c;
 int len;
