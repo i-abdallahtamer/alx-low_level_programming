76b448f978cb55520d96c47dad9eece69326e179
Smart Monday <uchemonday01@gmail.com>
2022-06-14 16:44:39 -0700

    function that prints all natural numbers from n to 98, followed by a new line

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* print_to_98 - greater, less, zero
+* @n: parameter to pass
+* Return: 1 if c is lowercase, 0 otherwise
+*/
+
+
+void print_to_98(int n)
+{
+if (n > 98)
+{
+while (n != 98)
+{
+_putchar (n % 10 + '0');
+n--;
+}
+else if (n < 98)
+{
+while (n != 98)
+{
+_putchar (n % 10 + '0');
+n++;
+}
+else
+{
+_putchar(n % 10 + '0');
+}
+}
