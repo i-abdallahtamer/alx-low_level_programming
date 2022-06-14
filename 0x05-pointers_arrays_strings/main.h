5884bc3c0e8caf00b2d5676dbca7e7ad7904779a
Smart Monday <uchemonday01@gmail.com>
2022-06-13 19:39:42 -0700

    function that prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
+void _puts(char *str);
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void print_array(int *a, int n);
 void rev_string(char *s);
 int _strlen(char *s);
+
