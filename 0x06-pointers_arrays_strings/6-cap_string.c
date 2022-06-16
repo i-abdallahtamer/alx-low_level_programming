2f9a1f6c27ec297de048689176b58f4bad4f9dcf
Smart Monday <uchemonday01@gmail.com>
2022-06-16 16:38:11 -0700

    function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,39 @@
 #include "main.h"
 
 /**
- * string_toupper - Change all lowercase letters to uppercase
- * @c: int pointer string variable
- * Return: c
+ * cap_string - capitalize all words of a string
+ * @str: pointer string variable
+ * Return: `str`
  */
 
-char *string_toupper(char *c)
+char *cap_string(char *str)
 {
-int len;
+	int i, c;
+	int sepa;
+	char nots[] = ",;.!?(){}\n\t\" ";
 
-for (len = 0; c[len] != '\0'; len++)
-{
-if (c[len] > 96 && c[len] < 123)
-{
-c[len] -= 32;
-}
+	for (i = 0, sepa = 0; str[i] != '\0'; i++)
+	{
+		if (str[0] > 96 && str[0] < 123)
+			sepa = 1;
+		for (c = 0; nots[c] != '\0'; c++)
+		{
+			if (nots[c] == str[i])
+				sepa = 1;
+		}
 
-}
-return (c);
+		if (sepa)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				str[i] -= 32;
+				sepa = 0;
+			}
+			else if (str[i] > 64 && str[i] < 91)
+				sepa = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				sepa = 0;
+		}
+	}
+	return (str);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
 char *cap_string(char *);
 void reverse_array(int *a, int n);
+char *cap_string(char *);
