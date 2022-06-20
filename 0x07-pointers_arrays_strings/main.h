246bf71571c3937fe86bebca118f89f8a0a6101b
Smart Monday <uchemonday01@gmail.com>
2022-06-20 16:58:30 -0700

    function that gets the length of a prefix substring.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 char *_strstr(char *haystack, char *needle);
+unsigned int _strspn(char *s, char *accept);
