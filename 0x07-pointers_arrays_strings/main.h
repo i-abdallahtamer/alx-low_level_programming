b65d255b33e9801858845150d17567a78af9ed79
Smart Monday <uchemonday01@gmail.com>
2022-06-20 17:54:10 -0700

    function that locates a character in a string.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 char *_strstr(char *haystack, char *needle);
 unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
