789df9e217f02f9e81818e86e3fbcc59b24ce877
Smart Monday <uchemonday01@gmail.com>
2022-06-22 18:36:31 -0700

    function that returns the length of a string.

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,11 @@
 
 int _strlen_recursion(char *s)
 {
-int len = 0;
 if (*s =='\0')
 {
-len +=1;
+return (0);
 }
-_strlen_recursion(s + 1);
-return (len);
+return (1 + _strlen_recursion(s + 1));
+
 
 }
