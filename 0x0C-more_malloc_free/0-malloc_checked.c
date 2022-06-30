5107f8cccfc54b5853c9f6d53a3211106d259bd3
Smart Monday <uchemonday01@gmail.com>
2022-06-30 16:34:28 -0700

    a function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
 /**
  * m - stores 3 int in a new allocated space in memory and prints the sum
  *
- *  
+ *
  * Return: nothing
  */
-
-
 
 void *malloc_checked(unsigned int b)
 {
 void *p;
 
- p = malloc(b);
- if (p == NULL)
- exit(98);
- return (p);
- 
-
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
 }
