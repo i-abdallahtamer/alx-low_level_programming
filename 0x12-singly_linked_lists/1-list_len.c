a5023c413243a10c664356cc2083b6152622831c
Smart Monday <uchemonday01@gmail.com>
2022-07-14 15:42:43 -0700

    Write a function that returns the number of elements in a linked list_t list.

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_list -print list.
+ * list_len -print list.
  *Description: Function that prints content of a single linked list
  * @h: pointer to a structure typedef list_t
  * Return: the number of nodes
  **/
 
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 const  list_t *temp = h;
 
@@ -18,11 +18,7 @@ size_t size;
 
 for (size = 0; temp != NULL; size++)
 {
-	if (temp->str == NULL)
-		printf("[0] (nill) \n");
-	
-	else
-		printf(" [%d] %s\n", temp->len, temp->str);
+
         temp = temp->next;
         
 }
