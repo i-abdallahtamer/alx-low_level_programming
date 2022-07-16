510255c046b922853fe187ef8e0c1ba281c89e0f
Smart Monday <uchemonday01@gmail.com>
2022-07-15 18:06:27 -0700

    Write a function that prints befor main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,8 +8,8 @@
 void firstRun(void) __attribute__ ((constructor));
 void firstRun(void)
 {
-printf("You're beat! and yet, you must allow,
-		\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,");
+printf("\nI bore my house upon my back!\n");
 
 
 }
