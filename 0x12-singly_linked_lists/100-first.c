763fb17027ad5c213f7ca443a3a6c664b0764c59
Smart Monday <uchemonday01@gmail.com>
2022-07-15 17:59:50 -0700

    Write a function that prints befor main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void firstRun (void) __attribute__ ((constructor));
+void firstRun(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+
+}
+
