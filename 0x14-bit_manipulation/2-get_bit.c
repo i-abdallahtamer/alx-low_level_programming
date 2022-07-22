deac18311276bf2eb16cbd21ac2d67f35859013c
Smart Monday <uchemonday01@gmail.com>
2022-07-22 14:14:34 -0700

    function that returns the value of a bit at a given index.

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * get_bit - function that prints the binary representation of a number.
+ * @n : unsigned long integer
+ * @index : is the index, starting from 0 of the bit you want to get
+ * Return: binary
+ */
+
+
+
+int get_bit(unsigned long int n, unsigned int index)
+
+{
+ char b;
+ unsigned int j;
+
+	j = (n >> index);
+	b = (j & 1);
+	if (b == 1 || b == 0)
+	{
+		return (b);
+	}
+	else
+	{
+		return (-1);
+	}
+}
